42919d404c338c8e4663b7a08bbc91a461acbb4f
ZhuoYifan <54524672+yifantop@users.noreply.github.com>
2021-09-22 14:36:20 +0800

    反转二叉树

---
+++ src/invertTree.js
@@ -0,0 +1,15 @@
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function (root) {
+  if (root === null) {
+    return root;
+  }
+  invertTree(root.left);
+  invertTree(root.right);
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  return root;
+};
